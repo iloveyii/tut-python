0d09a075c0875995f358987bd3ad09a54d549f79
Hazrat Ali <iloveyii@yahoo.com>
2019-12-22 23:39:10 +0100

    Added two objects

--- classes/Animal.py
+++ classes/Animal.py
@@ -1,15 +1,15 @@
 class Animal:
-    def __init__(self):
-        self.name = 'Animal'
-        self.color = 'Grey'
+    def __init__(self, name, color):
+        self.name = name  # 'Animal'
+        self.color = color  # 'Grey'
 
     def show(self):
         print(f'My name is {self.name} and I am {self.color}')
 
 
 # create objects
-cat = Animal()
+cat = Animal('Meo', 'silver')
 cat.show()
 
-dog = Animal()
+dog = Animal('Hund', 'brown')
 dog.show()
